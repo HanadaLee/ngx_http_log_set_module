17b5e85b705d030e23e9d7ccefca5009d33182e5
Hanada <im@hanada.info>
2025-01-19 10:18:55 +0000

    style: remove useless whiteline

--- ngx_http_log_var_set_module.c
+++ ngx_http_log_var_set_module.c
@@ -250,7 +250,6 @@ ngx_http_log_var_set_init(ngx_conf_t *cf)
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
     arr = &cmcf->phases[NGX_HTTP_LOG_PHASE].handlers;
-    
 
     h = ngx_array_push(arr);
     if (h == NULL) {
